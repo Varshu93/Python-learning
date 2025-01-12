27712b2a5d3716f789ef4195ae48cdcc6dd9e23e
Gokulraj <gokulraj5255@gmail.com>
2025-01-12 10:44:08 +0530

    Adding basics.py

---
+++ basics.py
@@ -0,0 +1,11 @@
+import requests
+
+
+url="https://jsonplaceholder.typicode.com/posts/2"
+print(url)
+
+response=requests.get(url)
+
+print(response.text)
+print(response.json())
+print(response.status_code)
