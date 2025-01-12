60fbfafb4974ff97da60b1fb6b3ad5b51704319c
Varshu93 <varshu.vaishu22@gmail.com>
2025-01-12 10:56:12 +0530

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def fetch_data():
+    url = "https://jsonplaceholder.typicode.com/posts/2"
+    response = requests.get(url)
+    return jsonify(response.json())
+
+if __name__ == "__main__":
+    app.run(debug=True)
